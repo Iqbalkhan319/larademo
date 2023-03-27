0d1ea871542866e90572219863c32da40e755bc0
Soremi Dare <daresoremi@gmail.com>
2023-03-27 13:05:00 -0400

    about us page added to the code

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -28,7 +28,16 @@
                             <a href="{{ route('register') }}" class="ml-4 font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">Sign up</a>
                         @endif
                     @endauth
-                    <a href="#" class="ml-4 font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">Account</a>
+                    <a href="#" 
+                    class="ml-4 font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white f
+                    ocus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">
+                    Abuot Us
+                   </a>
+                    <a href="#" 
+                    class="ml-4 font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white f
+                    ocus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">
+                    Account
+                   </a>
                 </div>
             @endif
 
